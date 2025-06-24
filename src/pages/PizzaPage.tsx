846a6fa706a2753291e47f1d47150255f500d2dc
Nikita Axyutin <tidyman94@gmail.com>
2025-06-24 16:32:29 +0500

    Переписал home для типизации, поменял логику рендера

--- src/pages/PizzaPage.tsx
+++ src/pages/PizzaPage.tsx
@@ -1,14 +1,24 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import Pizza from "../components/Pizza";
 import { useGetOneQuery, itemsApi } from "../redux/itemsApi";
 import Skeleton from "../components/Pizza/Skeleton";
+import { useEffect } from "react";
 
 function PizzaPage() {
 	console.log('render');
 	const { id } = useParams();
-	const { data, isLoading } = useGetOneQuery(id);
+	const navigate = useNavigate()
+	const { data } = useGetOneQuery(Number(id));
 
-	if (isLoading) return <Skeleton />;
+	useEffect(() => {
+		const isValideId = id !== undefined && Number(id) && Number(id) < 100 && Number(id) > 0
+		if (!isValideId) {
+			navigate('/')
+			alert('Такой пиццы нет. Перенаправление на главную')
+		}
+	}, [id, navigate])
+
+	if (!data) return <Skeleton />;
 
 	return <Pizza {...data[0]} isPage={true} />;
 }
