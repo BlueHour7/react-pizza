dbdb1a341a15f6fa4bf170d2e12d2142a2a8b99b
Nikita Axyutin <tidyman94@gmail.com>
2025-06-25 16:59:06 +0500

    Fixes

--- src/pages/PizzaPage.tsx
+++ src/pages/PizzaPage.tsx
@@ -4,15 +4,21 @@ import { useGetOneQuery, itemsApi } from "../redux/itemsApi";
 import Skeleton from "../components/Pizza/Skeleton";
 import { useEffect } from "react";
 
+// тут не оч хорошо сделано, потому что useGetOneQuery(Number(id), { skip: !isValideId()}) может вернуть NaN
+// но TS это пропустит потому что нан тоже число как бы. По сути у меня проверка есть, но не с точки зрения TS
+
 function PizzaPage() {
 	console.log('render');
 	const { id } = useParams();
 	const navigate = useNavigate()
-	const { data } = useGetOneQuery(Number(id));
+	const { data } = useGetOneQuery(Number(id), { skip: !isValideId()});
+
+	function isValideId() {
+		return id !== undefined && Number(id) && Number(id) < 100 && Number(id) > 0
+	}
 
 	useEffect(() => {
-		const isValideId = id !== undefined && Number(id) && Number(id) < 100 && Number(id) > 0
-		if (!isValideId) {
+		if (!isValideId()) {
 			navigate('/')
 			alert('Такой пиццы нет. Перенаправление на главную')
 		}
